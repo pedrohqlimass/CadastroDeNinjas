176e010fab25549331e9b01329bab6bb7e3caade
pedrohqlimass <pedroconicz@gmail.com>
2025-08-16 21:10:00 -0300

    feat: Cria o MissaoRepository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
